76aa6d86a60092c1ba41b038cb83b2127193f5cc
Vihaan Verma <vverma270705@gmail.com>
2025-08-28 20:09:01 +0530

    fix cors

--- api/server.js
+++ api/server.js
@@ -1,5 +1,6 @@
 // See https://github.com/typicode/json-server#module
 const jsonServer = require("json-server");
+
 const server = jsonServer.create();
 
 // Uncomment to allow write operations
@@ -15,25 +16,7 @@ const router = jsonServer.router(db);
 
 const middlewares = jsonServer.defaults();
 
-// Add CORS middleware BEFORE other middlewares
-server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Content-Type, Authorization, Content-Length, X-Requested-With"
-  );
-
-  // Handle preflight requests
-  if (req.method === "OPTIONS") {
-    res.sendStatus(200);
-  } else {
-    next();
-  }
-});
-
 server.use(middlewares);
-
 // Add this before server.use(router)
 server.use(
   jsonServer.rewriter({
@@ -41,9 +24,7 @@ server.use(
     "/blog/:resource/:id/show": "/:resource/:id",
   })
 );
-
 server.use(router);
-
 server.listen(3000, () => {
   console.log("JSON Server is running");
 });
